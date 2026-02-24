7e253898a35356b34882aa31ee4def45c04d4dbf
vishwas-droid <vishwasbajaj.global@gmail.com>
2026-02-24 23:17:38 +0530

    fix: handle versioned secret names during rotation

--- driver.go
+++ driver.go
@@ -463,7 +463,8 @@ func (d *SecretsDriver) updateServicesSecretReference(oldSecretName, newSecretNa
 		updatedSecrets := make([]*swarm.SecretReference, len(service.Spec.TaskTemplate.ContainerSpec.Secrets))
 
 		for i, secretRef := range service.Spec.TaskTemplate.ContainerSpec.Secrets {
-			if secretRef.SecretName == oldSecretName {
+			if secretRef.SecretName == oldSecretName ||
+				strings.HasPrefix(secretRef.SecretName, oldSecretName+"-") {
 				// Update to use the new secret name and ID
 				updatedSecrets[i] = &swarm.SecretReference{
 					File:       secretRef.File,
